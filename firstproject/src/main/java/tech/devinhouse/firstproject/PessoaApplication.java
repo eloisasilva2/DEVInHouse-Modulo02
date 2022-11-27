caf9510e53ed6e31f0789c57b5169ac319a799b6
eloisasilva2 <eloisa_silva2@estudante.sesisenai.org.br>
2022-11-27 11:22:43 -0300

    Semana 08 - Exercícios 4 a 10

---
+++ firstproject/src/main/java/tech/devinhouse/firstproject/PessoaApplication.java
@@ -0,0 +1,15 @@
+package tech.devinhouse.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaApplication.class, args);
+
+
+		
+	}
+
+}
